620d3116e260e70dee65dd14840e9c2b8cca43db
VanRar <vitalikf7@gmail.com>
2021-05-03 20:41:23 +0300

    поправил оформление

--- src/main/java/StreamMain.java
+++ src/main/java/StreamMain.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-
 public class StreamMain {
     public static void main(String[] args) {
         List<Integer> inList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
@@ -8,7 +7,6 @@ public class StreamMain {
         inList.stream()
                 .filter(x -> x > 0 & x % 2 == 0)
                 .sorted(Comparator.naturalOrder())
-                //.forEach(System.out::print);
-        .forEach(System.out::println);
+                .forEach(System.out::println);
     }
 }
